4c5710968c35faf9b5b09de73af52048084382a8
Jason Bisson <jasonbisson@hotmail.com>
2020-12-23 11:30:33 -0500

    Update alert policy

--- main.tf
+++ main.tf
@@ -10,7 +10,6 @@ provider "google" {
 }
 
 resource "google_project_service" "project_services" {
-  count                      = var.enable_apis ? length(var.activate_apis) : 0
   service                    = element(var.activate_apis, count.index)
   disable_on_destroy         = var.disable_services_on_destroy
   disable_dependent_services = var.disable_dependent_services
@@ -18,7 +17,6 @@ resource "google_project_service" "project_services" {
 
 resource "google_logging_metric" "log_alert_metric" {
   for_each = {for item in local.logAlerts: item.name => item}
-
   name   = each.value.name
   filter = each.value.rule
   metric_descriptor {
